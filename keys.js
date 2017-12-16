977e59c39703b0850ba55d69f9603ae9138f7c87
Brian Cocherell <brian.cocherell@gmail.com>
2017-12-16 13:46:58 -0500

    initial file creation

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'OUEHfaER0Unt1qPJ0liRqEYci',
+  consumer_secret: 'QdkmxgzgUOBi2nMjqTV7puYAKg5N4r1PFQWiqFzlkSDJ9Ew2cT',
+  access_token_key: '942099628530520064-cFQNVF5sfksbwDS8SXPOCkKdFDRvOXY',
+  access_token_secret: 'Lxcqf3cOjwYJPDTbZ2vOYKRmcTJRPEn5ZuGKlstg2DCBh',
+}
+
+module.exports = twitterKeys;
